9cf0fad61677b9a35ad5a51dfd2722675f10c6b2
Abolfazl <aabolfazlabdollahi@gmail.com>
2026-08-15 16:43:39 +0330

    refactor plhgin

--- class-block.php
+++ class-block.php
@@ -1,8 +1,16 @@
 <?php
 
-use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-final class Zibal_Gateway_Blocks extends AbstractPaymentMethodType
+$zibal_abstract_payment_method_class = 'Automattic\\WooCommerce\\Blocks\\Payments\\Integrations\\AbstractPaymentMethodType';
+
+if (!class_exists('Zibal_Abstract_Payment_Method_Type', false)) {
+    class_alias($zibal_abstract_payment_method_class, 'Zibal_Abstract_Payment_Method_Type');
+}
+
+final class Zibal_Gateway_Blocks extends Zibal_Abstract_Payment_Method_Type
 {
 
     private $gateway;
@@ -24,33 +32,40 @@ final class Zibal_Gateway_Blocks extends AbstractPaymentMethodType
 
         wp_register_script(
             'zibal_gateway-blocks-integration',
-            plugin_dir_url(__FILE__) . '/assets/js/zibal-checkout.js',
-            [
+            plugin_dir_url(__FILE__) . 'assets/js/zibal-checkout.js',
+            array(
                 'wc-blocks-registry',
                 'wc-settings',
                 'wp-element',
                 'wp-html-entities',
                 'wp-i18n',
-            ],
-            null,
+            ),
+            defined('WOO_ZIBAL_VERSION') ? WOO_ZIBAL_VERSION : '2.1.0',
             true
         );
         if (function_exists('wp_set_script_translations')) {
-            wp_set_script_translations('zibal_gateway-blocks-integration');
+            wp_set_script_translations(
+                'zibal_gateway-blocks-integration',
+                'zibal-woocommerce',
+                WOO_GAPIRDIRZIBAL . 'languages'
+            );
 
         }
-        return ['zibal_gateway-blocks-integration'];
+        return array('zibal_gateway-blocks-integration');
     }
 
     public function get_payment_method_data()
     {
-        return [
-            'title' => $this->gateway->title,
-            'description' => $this->gateway->description,
-            'icon' => plugin_dir_url(__FILE__) . '/assets/images/logo.png',
-            'supports' => array_values($this->gateway->supports),
-        ];
+        $supports = isset($this->gateway->supports) && is_array($this->gateway->supports)
+            ? array_values($this->gateway->supports)
+            : array();
+
+        return array(
+            'title' => wp_strip_all_tags((string) $this->gateway->title),
+            'description' => wp_strip_all_tags((string) $this->gateway->description),
+            'icon' => plugin_dir_url(__FILE__) . 'assets/images/logo.png',
+            'supports' => $supports,
+        );
     }
 
 }
-?>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 /*
-  Plugin Name: Gateway zibal for Woocommerce
-  Version: 2.0
-  Description: این افزونه درگاه زیبال برای فروشگاه ساز ووکامرس میباشد.
-  Author: zibal
-  Author URI: https://zibal.ir
-  Stable tag: 2.0
+Plugin Name: Gateway Zibal for WooCommerce
+Description: درگاه پرداخت زیبال برای فروشگاه‌ساز ووکامرس.
+Version: 2.1.0
+Requires at least: 4.7
+Requires PHP: 5.6
+WC requires at least: 3.0
+WC tested up to: 10.8
+Author: Zibal
+Author URI: https://zibal.ir
+Text Domain: zibal-woocommerce
+Domain Path: /languages
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 */
@@ -15,27 +20,39 @@ if (!defined('ABSPATH'))
 
 define('WOO_GAPIRDIRZIBAL', plugin_dir_path(__FILE__));
 define('WOO_GAPIRDUZIBAL', plugin_dir_url(__FILE__));
+define('WOO_ZIBAL_VERSION', '2.1.0');
+
+function Woocommerce_Add_zibal_Gateway($methods)
+{
+    $methods[] = 'WC_Gateway_Zibal';
+    return $methods;
+}
+
+function zibal_woocommerce_load_textdomain()
+{
+    load_plugin_textdomain(
+        'zibal-woocommerce',
+        false,
+        dirname(plugin_basename(__FILE__)) . '/languages'
+    );
+}
 
 function load_zibal_woo_gateway()
 {
-
     add_filter('woocommerce_payment_gateways', 'Woocommerce_Add_zibal_Gateway');
-    function Woocommerce_Add_zibal_Gateway($methods)
-    {
-        $methods[] = 'WC_Gateway_Zibal';
-        return $methods;
-    }
     require_once(WOO_GAPIRDIRZIBAL . 'class-wc-gateway-zibal.php');
-
 }
+add_action('plugins_loaded', 'zibal_woocommerce_load_textdomain', 0);
 add_action('plugins_loaded', 'load_zibal_woo_gateway', 0);
 
 function declare_zibal_cart_checkout_blocks_compatibility()
 {
 
-    if (class_exists('\Automattic\WooCommerce\Utilities\FeaturesUtil')) {
-        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('cart_checkout_blocks', __FILE__, true);
-        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+    $features_util = 'Automattic\\WooCommerce\\Utilities\\FeaturesUtil';
+
+    if (class_exists($features_util)) {
+        call_user_func(array($features_util, 'declare_compatibility'), 'cart_checkout_blocks', __FILE__, true);
+        call_user_func(array($features_util, 'declare_compatibility'), 'custom_order_tables', __FILE__, true);
     }
 }
 
@@ -51,10 +68,10 @@ function zibal_register_order_approval_payment_method_type()
 
     require_once plugin_dir_path(__FILE__) . 'class-block.php';
 
-    add_action(
-        'woocommerce_blocks_payment_method_type_registration',
-        function (Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry) {
-            $payment_method_registry->register(new zibal_Gateway_Blocks);
-        }
-    );
+    add_action('woocommerce_blocks_payment_method_type_registration', 'zibal_register_blocks_payment_method');
+}
+
+function zibal_register_blocks_payment_method($payment_method_registry)
+{
+    $payment_method_registry->register(new Zibal_Gateway_Blocks());
 }
